52770941637f2089140f1d652ce649e12549ac2f
Seto Elkahfi <setoelkahfi@gmail.com>
2017-03-28 19:24:10 +0700

    Change example row

--- Example/JomloTableView/ExampleRow.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ExampleRow.swift
-//  JomloTableView
-//
-//  Created by SDMobile on 3/28/17.
-//  Copyright © 2017 CocoaPods. All rights reserved.
-//
-
-import Foundation
---
+++ Example/JomloTableView/SimpleCell.swift
@@ -0,0 +1,21 @@
+//
+//  SimpleCell.swift
+//  JomloTableView
+//
+//  Created by SDMobile on 3/28/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class SimpleCell: JomloTableViewCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
