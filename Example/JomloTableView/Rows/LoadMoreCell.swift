8865ae3a462bca12ae143e8bd491a9e8f8a43e13
Seto Elkahfi <setoelkahfi@gmail.com>
2017-04-08 17:03:06 +0700

    Add load more example row

---
+++ Example/JomloTableView/Rows/LoadMoreCell.swift
@@ -0,0 +1,24 @@
+//
+//  LoadMoreCell.swift
+//  JomloTableView
+//
+//  Created by SDMobile on 4/5/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class LoadMoreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Example/JomloTableView/Rows/LoadMoreRow.swift
@@ -0,0 +1,9 @@
+//
+//  LoadMoreRow.swift
+//  JomloTableView
+//
+//  Created by SDMobile on 4/5/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
