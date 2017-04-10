e3e147076be11d353b9b6f8bf61c373b2979d26c
Seto Elkahfi <setoelkahfi@gmail.com>
2017-04-10 13:22:31 +0700

    Add simple table view controller

---
+++ Example/JomloTableView/View Controllers/SimpleTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SimpleTableViewController.swift
+//  JomloTableView
+//
+//  Created by SDMobile on 4/10/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class SimpleTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
